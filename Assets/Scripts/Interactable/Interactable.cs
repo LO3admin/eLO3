a414f0697ed65d9c47bfb421e1b94540affe3956
drzezga <19654689+drzezga@users.noreply.github.com>
2021-03-20 23:25:59 +0100

    Added interactions with doors

---
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Interactable GameObjects can be interacted with by the player.
+/// A prompt will be shown with a key to press to interact with this object.
+/// </summary>
+public abstract class Interactable : MonoBehaviour
+{
+    // Every item with Interactable also needs a collider with a tag "Interactable"
+    public virtual void Interact() { }
+
+    public virtual void Interact(GameObject interactor)
+    {
+        Interact();
+    }
+
+}
---
+++ Assets/Scripts/Interactable/PlayerInteract.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _playerCamera = default;
+
+    [SerializeField]
+    private GameObject _interactPrompt = default;
+
+    [Header("Options")]
+    [SerializeField]
+    private float _interactDistance = default;
+
+    [SerializeField]
+    [Tooltip("Interact cooldown in seconds")]
+    private float _interactCooldown = default;
+
+    private float _nextInteractTime = 0;
+
+    void Update() {
+        if (Input.GetButtonDown("Use")) {
+            OnInteract();
+        } else {
+            // Check if an interactable is in front of the player
+            Ray ray = new Ray(_playerCamera.transform.position, _playerCamera.transform.forward * _interactDistance);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, _interactDistance))
+            {
+                if (hit.collider.tag == "Interactable")
+                {
+                    ShowPrompt();
+                } else
+                {
+                    HidePrompt();
+                }
+            } else
+            {
+                HidePrompt();
+            }
+        }
+    }
+
+    private void OnInteract() {
+        if (Time.time < _nextInteractTime)
+        {
+            return;
+        }
+        _nextInteractTime = Time.time + _interactCooldown;
+
+        Ray ray = new Ray(_playerCamera.transform.position, _playerCamera.transform.forward * _interactDistance);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, _interactDistance))
+        {
+            if (hit.collider.tag == "Interactable")
+            {
+                Interactable interactable = hit.collider.gameObject.GetComponent<Interactable>();
+
+                interactable?.Interact(gameObject);
+            }
+        }
+    }
+
+    // TODO: Do this with an animator or a tweening library
+    private void ShowPrompt()
+    {
+        _interactPrompt.SetActive(true);
+    }
+
+    private void HidePrompt()
+    {
+        _interactPrompt.SetActive(false);
+    }
+}
